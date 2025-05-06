a5b83885c9c5f5cf5a9757acad4df1e45784390a
yamini-Krishna <yaminimusku04@gmail.com>
2025-05-06 19:35:23 +0530

    3.2.1

--- day2/3.Object-Oriented Design/2_inheritance_super/subclassing.py
+++ day2/3.Object-Oriented Design/2_inheritance_super/subclassing.py
@@ -1,1 +1,12 @@
+class Book:
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+
+class Novel(Book):
+    pass
+
+n = Novel("1984", "George Orwell")
+print(n.title, "-", n.author)
+# Output: 1984 - George Orwell
 
