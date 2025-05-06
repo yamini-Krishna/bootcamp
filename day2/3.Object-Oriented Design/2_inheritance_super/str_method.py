e0183185a0837c523d6939c13fba1530e309b04d
yamini-Krishna <yaminimusku04@gmail.com>
2025-05-06 19:37:03 +0530

    3.2.5

---
+++ day2/3.Object-Oriented Design/2_inheritance_super/str_method.py
@@ -0,0 +1,11 @@
+class Book:
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+
+    def __str__(self):
+        return f"'{self.title}' by {self.author}"
+
+b = Book("1984", "George Orwell")
+print(b)
+# Output: '1984' by George Orwell
