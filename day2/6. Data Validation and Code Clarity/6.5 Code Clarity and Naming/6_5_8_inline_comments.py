4cafe62215f624c8da9759c168c24449ef5700f3
yamini-Krishna <yaminimusku04@gmail.com>
2025-05-07 10:04:56 +0530

    5.8

---
+++ day2/6. Data Validation and Code Clarity/6.5 Code Clarity and Naming/6_5_8_inline_comments.py
@@ -0,0 +1,9 @@
+def calculate_area(length, width):
+    # Multiply length and width to get the area
+    return length * width
+
+# Example usage
+length = 5
+width = 3
+area = calculate_area(length, width)
+print(f"Area: {area}")  # Expected Output: Area: 15
