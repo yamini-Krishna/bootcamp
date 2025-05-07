c6d9606d2a01c3f2dce0bca02f99264dc48fd0f0
yamini-Krishna <yaminimusku04@gmail.com>
2025-05-07 10:02:26 +0530

    5.3

---
+++ day2/6. Data Validation and Code Clarity/6.5 Code Clarity and Naming/6_5_2_constants.py
@@ -0,0 +1,11 @@
+
+
+MAX_RETRIES = 3
+
+def connect_to_server():
+    retries = 0
+    while retries < MAX_RETRIES:
+        print(f"Attempt {retries + 1}")
+        retries += 1
+
+connect_to_server()  # Expected Output: Attempt 1, Attempt 2, Attempt 3
