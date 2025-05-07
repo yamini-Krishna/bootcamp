2cb0afc384c1f7cc16f684e6aae890b5c5adaf79
yamini-Krishna <yaminimusku04@gmail.com>
2025-05-07 10:04:38 +0530

    5.7

---
+++ day2/6. Data Validation and Code Clarity/6.5 Code Clarity and Naming/6_5_7_module_level_docstring.py
@@ -0,0 +1,13 @@
+
+
+def calculate_area(length, width):
+    return length * width
+
+def display_area(area):
+    print(f"Area: {area}")
+
+# Example usage
+length = 5
+width = 3
+area = calculate_area(length, width)
+display_area(area)  # Expected Output: Area: 15
