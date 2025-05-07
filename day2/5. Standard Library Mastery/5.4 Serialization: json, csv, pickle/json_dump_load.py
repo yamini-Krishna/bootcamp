dd01fa684f86dce12d1a5e031e10e0a835b1093e
yamini-Krishna <yaminimusku04@gmail.com>
2025-05-07 09:35:12 +0530

    4.1

--- day2/5. Standard Library Mastery/5.4 Serialization: json, csv, pickle/json_dump_load.py
+++ day2/5. Standard Library Mastery/5.4 Serialization: json, csv, pickle/json_dump_load.py
@@ -2,7 +2,9 @@ import json
 
 data = {"name": "Alice", "age": 25}
 json_str = json.dumps(data)
-print("JSON:", json_str)
+print("JSON:", json_str)          # JSON: {"name": "Alice", "age": 25}
 
 loaded = json.loads(json_str)
-print("Loaded:", loaded)
+print("Loaded:", loaded)          # Loaded: {'name': 'Alice', 'age': 25}
+
+
