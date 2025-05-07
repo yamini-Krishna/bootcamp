674ddd0e423622a82cdc6b1b06be98858aae5d4f
yamini-Krishna <yaminimusku04@gmail.com>
2025-05-07 10:11:42 +0530

    -

--- day2/7. Performance and Debugging/7.1 Profiling and Timing/7_1_3_cprofile_example.py
+++ day2/7. Performance and Debugging/7.1 Profiling and Timing/7_1_3_cprofile_example.py
@@ -6,3 +6,12 @@ def expensive_function():
     return total
 
 expensive_function()
+
+
+expected output:
+
+         4 function calls in 0.125 seconds
+         1    0.000    0.000    0.125    0.125 <script_name>.py:4(expensive_function)
+         1    0.000    0.000    0.000    0.000 <string>:1(<module>)
+         1    0.000    0.000    0.000    0.000 {built-in method builtins.exec}
+         1    0.000    0.000    0.000    0.000 {method 'disable' of '_lsprof.Profiler' objects}
