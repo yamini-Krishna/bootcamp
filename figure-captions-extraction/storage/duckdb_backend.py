1fa25d148a7ac31c06aef06207ea45ba7c0f21ca
yamini-Krishna <yaminimusku04@gmail.com>
2025-05-29 17:50:12 +0530

    Update duckdb_backend.py

--- figure-captions-extraction/storage/duckdb_backend.py
+++ figure-captions-extraction/storage/duckdb_backend.py
@@ -1,44 +1,4 @@
-# storage/duckdb_backend.py
-'''
-import duckdb
-from .base import StorageBackend
-import os
 
-class DuckDBStorage(StorageBackend):
-    def __init__(self, db_path="data/articles.duckdb"):
-        os.makedirs(os.path.dirname(db_path), exist_ok=True)
-        self.conn = duckdb.connect(db_path)
-        self.conn.execute("""
-            CREATE TABLE IF NOT EXISTS articles (
-                pmcid TEXT PRIMARY KEY,
-                title TEXT,
-                journal TEXT,
-                abstract TEXT,
-                authors TEXT
-            )
-        """)
-
-    def save(self, data: dict):
-        self.conn.execute("""
-            INSERT OR REPLACE INTO articles
-            (pmcid, title, journal, abstract, authors)
-            VALUES (?, ?, ?, ?, ?)
-        """, (
-            data["pmcid"],
-            data.get("title"),
-            data.get("journal"),
-            data.get("abstract"),
-            ", ".join(data.get("authors", []))
-        ))
-
-    def query_all(self) -> list:
-        return self.conn.execute("SELECT * FROM articles").fetchall()
-
-    def clear(self):
-        self.conn.execute("DELETE FROM articles")
-'''
-
-# storage/duckdb_backend.py
 import duckdb
 from .base import StorageBackend
 import os
